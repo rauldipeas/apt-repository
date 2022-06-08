13bcf5acf4ff0749fd1a4a63604f68d13454463b
Raul Dipeas <rauldipeas@disroot.org>
2022-06-08 19:48:31 -0300

    ^getpkg

--- scripts/getpkg/sfizz.sh
+++ scripts/getpkg/sfizz.sh
@@ -3,7 +3,5 @@ set -e
 # Sfizz
 rm -rf sfizz*.deb
 aria2c --console-log-level=error --summary-interval=0\
-	"$(wget -qO-\
-	'http://download.opensuse.org/repositories/home:/sfztools:/sfizz/xUbuntu_20.04/amd64/'\
-	|grep amd64.deb|cut -d '"' -f4|tail -n1)"
+	http://download.opensuse.org/repositories/home:/sfztools:/sfizz/xUbuntu_20.04/amd64/"$(wget -qO- 'http://download.opensuse.org/repositories/home:/sfztools:/sfizz/xUbuntu_20.04/amd64/'|grep amd64.deb|cut -d '"' -f4|tail -n1)"
 rm -rf sfizz*.metalink
