5cfdf6bde3a45832a35d91d141563bd6f99ff301
Raul Dipeas <rauldipeas@disroot.org>
2022-06-13 05:19:38 -0300

    ^getpkg

--- scripts/getpkg/lsd.sh
+++ scripts/getpkg/lsd.sh
@@ -5,4 +5,4 @@ rm -rf lsd*.deb
 aria2c --console-log-level=error --summary-interval=0\
 	"$(wget -qO-\
 	https://api.github.com/repos/Peltoche/lsd/releases|\
-	grep browser_download_url|grep download|grep amd64.deb|head -n1|cut -d '"' -f4)"
+	grep browser_download_url|grep download|grep amd64.deb|grep -v musl|head -n1|cut -d '"' -f4)"
