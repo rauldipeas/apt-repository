d1ae4b4d38b5e7724c774b6733b92f0f2a55c39b
Raul Dipeas <rauldipeas@disroot.org>
2022-06-19 00:06:50 -0300

    ^actions-cleanup

--- actions-cleanup.sh
+++ actions-cleanup.sh
@@ -3,6 +3,6 @@ set -e
 PROJECT='rauldipeas/apt-repository'
 KEEP=1
 gh api\
-	repos/$PROJECT/actions/runs\
+	repos/"$PROJECT"/actions/runs\
 	--paginate -q '.workflow_runs[]|select(.head_branch != "any")|"\(.id)"'|tail -n+$((KEEP+1))\
-    |xargs -n1 -I % gh api repos/$PROJECT/actions/runs/% -X DELETE
+    |xargs -n1 -I % gh api repos/"$PROJECT"/actions/runs/% -X DELETE
