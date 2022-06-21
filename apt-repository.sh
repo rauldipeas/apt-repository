ef1f279a6050922c938a11d2123450bc9b257453
Raul Dipeas <rauldipeas@disroot.org>
2022-06-21 00:35:05 -0300

    ^readme

--- apt-repository.sh
+++ apt-repository.sh
@@ -3,5 +3,6 @@ set -e
 echo \
 'deb [trusted=yes] https://rauldipeas.fury.site/apt/ * *'|\
 sudo tee \
-/etc/apt/sources.list.d/rauldipeas.list
+/etc/apt/sources.list.d/rauldipeas.list\
+>/dev/null
 sudo apt update 2>/dev/null
