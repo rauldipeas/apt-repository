8c437d8aacd7b17556e885e6b617d60d801e87c4
Raul Dipeas <rauldipeas@disroot.org>
2022-06-22 17:20:55 -0300

    ^getpkg

--- scripts/getpkg/freezer.sh
+++ scripts/getpkg/freezer.sh
@@ -5,7 +5,10 @@ rm -rf\
 	freezer*.deb\
 	libssl1.1*.deb
 aria2c --console-log-level=error --summary-interval=0\
-	http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.13_amd64.deb
+	http://security.ubuntu.com/ubuntu/pool/main/o/openssl/"$(\
+	wget -qO-\
+		http://security.ubuntu.com/ubuntu/pool/main/o/openssl/|\
+		grep libssl1.1_|grep amd64.deb|head -n1|cut -d '"' -f8)"
 aria2c --console-log-level=error --summary-interval=0\
 	"$(wget -qO-\
 	https://freezerapk.com/download|grep deb|cut -d '"' -f6)"
