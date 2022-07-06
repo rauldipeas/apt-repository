4b74e90ffa2c3f1183db56b7f9f09ad89bb99895
Raul Dipeas <rauldipeas@disroot.org>
2022-07-06 10:54:11 -0300

    ^getpkg

--- scripts/getpkg/bitwarden.sh
+++ scripts/getpkg/bitwarden.sh
@@ -2,5 +2,9 @@
 set -e
 # Bitwarden
 rm -rf Bitwarden*.deb
+#aria2c --console-log-level=error --summary-interval=0\
+#    'https://vault.bitwarden.com/download/?app=desktop&platform=linux&variant=deb'
 aria2c --console-log-level=error --summary-interval=0\
-    'https://vault.bitwarden.com/download/?app=desktop&platform=linux&variant=deb'
+	"$(wget -qO-\
+	https://api.github.com/repos/bitwarden/clients/releases|\
+	grep browser_download_url|grep all.deb|head -n1|cut -d '"' -f4)"    
