52a709a80c172a682932076e3e44d6034f6158fa
Raul Dipeas <rauldipeas@disroot.org>
2022-07-16 22:43:34 -0300

    ^getpkg

--- scripts/getpkg/gitqlient.sh
+++ scripts/getpkg/gitqlient.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-# Heroic
+# GitQlient
 aria2c --console-log-level=error --summary-interval=0\
-    "$(wget -qO- https://api.github.com/repos/francescmm/GitQlient/releases|\
+    "$(wget -qO-\
+    https://api.github.com/repos/francescmm/GitQlient/releases|\
     grep browser_download_url|grep amd64.deb|head -n1|cut -d '"' -f4)"
