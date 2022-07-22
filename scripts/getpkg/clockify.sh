6de320ea8866c878f33089cf464cdd1d0fc883ed
Raul Dipeas <rauldipeas@disroot.org>
2022-07-22 16:13:22 -0300

    ^getpkg

---
+++ scripts/getpkg/clockify.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+# Clockify
+aria2c --console-log-level=error --summary-interval=0\
+    "$(wget -qO-\
+    https://clockify.me/linux-time-tracking|\
+    grep .deb|cut -d '"' -f4|head -n1)"
--- scripts/getpkg/sfizz.sh
+++ scripts/getpkg/sfizz.sh
@@ -4,5 +4,5 @@ set -e
 aria2c --console-log-level=error --summary-interval=0\
 	 	"$(wget -qO-\
 		'https://software.opensuse.org/download.html?project=home%3Asfztools%3Asfizz&package=sfizz'|\
-	 	grep amd64.deb|grep 20.04|cut -d '"' -f2)"
+	 	grep amd64.deb|grep 22.04|cut -d '"' -f2)"
 rm -rf sfizz*.metalink
