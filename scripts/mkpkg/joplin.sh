01c44c4e3c392d39a433c1d46159d9e149c76b67
Raul Dipeas <rauldipeas@disroot.org>
2022-07-25 23:02:56 -0300

    ^mkpkg

--- scripts/mkpkg/joplin.sh
+++ scripts/mkpkg/joplin.sh
@@ -5,7 +5,7 @@ git clone -q\
 cd joplin-deb
 sudo apt install -y containerd docker.io runc
 sudo gpasswd -a "$USER" docker
-mkdir -pv "$HOME"/Downloads
+#mkdir -pv "$HOME"/Downloads
 JOPLIN_TAG="$(wget -qO- https://api.github.com/repos/laurent22/joplin/tags|\
         grep tag|head -n1|cut -d '"' -f4|sed 's@https://api.github.com/repos/laurent22/joplin/zipball/refs/tags/v@@g')"
 ./joplin-deb.sh "$JOPLIN_TAG"
