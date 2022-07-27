534247921ce0391f77d1ff540b8e696621550f36
Raul Dipeas <rauldipeas@disroot.org>
2022-07-27 16:29:33 -0300

    ^mkpkg ^readme

---
+++ scripts/mkpkg/clickup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+# ClickUp #TODO
+git clone -q\
+    https://aur.archlinux.org/clickup.git
+cd fluent-reader-bin
+sed -i\
+    "s/depends=('gtk3' 'libindicator-gtk2' 'dbus-glib' 'alsa-lib' 'libdbusmenu-gtk2' 'nss')//g"\
+    PKGBUILD
+makedeb -d #-H 'Section: office'
+cd ..
+cp\
+    clickup/clickup*.deb\
+    assets/packages/
+rm -rf clickup*
