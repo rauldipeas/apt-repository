2b6a2b4f9a13e261333a2a0b8fec8cac9ede6826
Raul Dipeas <rauldipeas@disroot.org>
2022-08-12 13:39:37 -0300

    ^getpkg

--- scripts/getpkg/clockify.sh
+++ scripts/getpkg/clockify.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 # Clockify
+#aria2c --console-log-level=error --summary-interval=0\
+#    "$(wget -qO-\
+#    https://clockify.me/linux-time-tracking|\
+#    grep .deb|cut -d '"' -f4|head -n1)"
 aria2c --console-log-level=error --summary-interval=0\
-    "$(wget -qO-\
-    https://clockify.me/linux-time-tracking|\
-    grep .deb|cut -d '"' -f4|head -n1)"
+    https://clockify.me/downloads/Clockify_Setup.deb
