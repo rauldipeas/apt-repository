58ba6d88ec32d183c3b6f5020f53145b18c4ffe3
Raul Dipeas <rauldipeas@disroot.org>
2022-10-16 19:57:21 -0300

    -lsd -micro -stremio -system-monitoring-center

--- scripts/getpkg/lsd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-# LSD
-aria2c --console-log-level=error --summary-interval=0\
-	"$(wget -qO-\
-	https://api.github.com/repos/Peltoche/lsd/releases|\
-	grep browser_download_url|grep download|grep amd64.deb|grep -v musl|head -n1|cut -d '"' -f4)"
--- scripts/getpkg/system-monitoring-center.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-# System Monitoring Center
-aria2c --console-log-level=error --summary-interval=0\
-	"$(wget -qO-\
-	https://api.github.com/repos/hakandundar34coding/system-monitoring-center/releases|\
-	grep browser_download_url|grep all.deb|head -n1|cut -d '"' -f4)"
