84bca8388d24a96d716c7ec3002842ffa8f4ee95
Raul Dipeas <rauldipeas@disroot.org>
2022-10-20 10:50:20 -0300

    ^scripts

--- scripts/getpkg/stremio.sh
+++ scripts/getpkg/stremio.sh
@@ -7,6 +7,6 @@ aria2c --console-log-level=error --summary-interval=0\
 	http://ftp.debian.org/debian/pool/non-free/f/fdk-aac/|\
 	grep libfdk-aac1|grep amd64.deb|head -n2|tail -n1|cut -d '"' -f8)"
 #aria2c --console-log-level=error --summary-interval=0\
-	"$(wget -qO-\
-	https://www.stremio.com/downloads|\
-	grep amd64.deb|cut -d '"' -f222)"
+#	"$(wget -qO-\
+#	https://www.stremio.com/downloads|\
+#	grep amd64.deb|cut -d '"' -f222)"
