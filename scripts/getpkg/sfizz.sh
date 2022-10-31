85271ac956688e8189c8aac75f0faa931a328d70
Raul Dipeas <rauldipeas@disroot.org>
2022-10-31 11:31:33 +0000

    ^scripts

--- scripts/getpkg/sfizz.sh
+++ scripts/getpkg/sfizz.sh
@@ -5,4 +5,3 @@ aria2c --console-log-level=error --summary-interval=0\
 	 	"$(wget -qO-\
 		'https://software.opensuse.org/download.html?project=home%3Asfztools%3Asfizz&package=sfizz'|\
 	 	grep amd64.deb|grep 22.04|cut -d '"' -f2)"
-rm -r sfizz*.metalink
