468e768aedb99a447b51780564993404b752bba4
Raul Dipeas <rauldipeas@disroot.org>
2022-11-06 22:58:24 +0000

    ^apt-repository

--- apt-repository.sh
+++ apt-repository.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 echo 'deb [trusted=yes] https://rauldipeas.fury.site/apt/ * * #Raul Dipeas'|sudo tee /etc/apt/sources.list.d/rauldipeas.list>/dev/null
-sudo apt update 2>/dev/null
+pkcon refresh 2>/dev/null
