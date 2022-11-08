60bacba0636b38f4bf61394cfae86ffdd0231672
Raul Dipeas <rauldipeas@disroot.org>
2022-11-08 00:42:44 +0000

    ^apt-repository ^readme

--- apt-repository.sh
+++ apt-repository.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 set -e
-echo 'deb [trusted=yes] https://rauldipeas.fury.site/apt/ * * #Raul Dipeas'|sudo tee /etc/apt/sources.list.d/rauldipeas.list>/dev/null
+rm -f /etc/apt/sources.list.d/rauldipeas.list
+cat <<EOF |sudo tee /etc/apt/sources.list.d/rauldipeas.sources>/dev/null
+X-Repolib-Name: Raul Dipeas
+Enabled: yes
+Types: deb
+URIs: https://rauldipeas.fury.site/apt
+Suites: *
+Components: *
+Trusted: yes
+EOF
 pkcon refresh 2>/dev/null
