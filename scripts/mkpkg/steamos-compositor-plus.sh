1c0fb3b686dac60635aec8d95b03c3f0ad93a6a4
Raul Dipeas <rauldipeas@disroot.org>
2022-11-28 18:10:34 -0300

    ^apt-repository ^scripts

---
+++ scripts/mkpkg/steamos-compositor-plus.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+# Steam OS compositor plus
+git clone -q https://github.com/ChimeraOS/steamos-compositor-plus
+cd steamos-compositor-plus
+apt-fast install -y\
+    automake1.11\
+    debhelper\
+    devscripts\
+    libgl1-mesa-dev\
+    libsdl-image1.2-dev\
+    libudev-dev\
+    libx11-dev\
+    libxcomposite-dev\
+    libxdamage-dev\
+    libxrandr-dev\
+    libxrender-dev\
+    libxxf86vm-dev
+dpkg-buildpackage -b -us -uc
+cd ..
+mv steamos-compositor-plus*.deb assets/packages
+rm -r steamos-compositor-plus*
