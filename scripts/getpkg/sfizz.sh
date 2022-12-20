d0130b458ccf7462755f085f6c422141d615ac9a
Raul Dipeas <rauldipeas@disroot.org>
2022-12-20 01:40:32 -0300

    ^scripts

--- scripts/getpkg/sfizz.sh
+++ scripts/getpkg/sfizz.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 # Sfizz
-wget -q --show-progress\
-	 	"$(wget -qO-\
-		'https://software.opensuse.org/download.html?project=home%3Asfztools%3Asfizz&package=sfizz'|\
-	 	grep amd64.deb|grep 22.04|cut -d '"' -f2)"
+#wget -q --show-progress\
+#	 	"$(wget -qO-\
+#		'https://software.opensuse.org/download.html?project=home%3Asfztools%3Asfizz&package=sfizz'|\
+#	 	grep amd64.deb|grep 22.04|cut -d '"' -f2)"
