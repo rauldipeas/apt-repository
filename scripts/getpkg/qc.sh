0e2dce6bdcb27652d4e3736589fad0b7efc3f08e
Raul Dipeas <rauldipeas@disroot.org>
2022-12-23 10:50:50 -0300

    ^scripts

--- scripts/getpkg/qc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-# qc
-wget -q --show-progress\
-	"$(wget -qO-\
-	https://api.github.com/repos/qownnotes/qc/releases|\
-	grep browser_download_url|grep amd64.deb|head -n1|cut -d '"' -f4)"
