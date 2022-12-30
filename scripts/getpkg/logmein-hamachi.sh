2da112ab0f97fc5ace937a29996905a2f05a014f
Raul Dipeas <rdipeas+github@proton.me>
2022-12-30 11:49:12 -0300

    ^scripts

--- scripts/getpkg/logmein-hamachi.sh
+++ scripts/getpkg/logmein-hamachi.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 # Logmein Hamachi
-wget -q --show-progress https://www.vpn.net$(wget -qO- https://www.vpn.net/linux|grep amd64.deb|cut -d '"' -f6)
+wget -q --show-progress https://vpn.net$(wget -qO- https://vpn.net/linux|grep amd64.deb|cut -d '"' -f6)
