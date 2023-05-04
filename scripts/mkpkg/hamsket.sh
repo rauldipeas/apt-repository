e55006fc9bc2dbc7d40115f5c79289cd136acc25
Raul Dipeas <rdipeas+github@proton.me>
2023-05-04 11:16:29 -0300

    ^scripts

--- scripts/mkpkg/hamsket.sh
+++ scripts/mkpkg/hamsket.sh
@@ -2,9 +2,9 @@
 set -e
 # Hasmsket
 wget -q --show-progress "$(wget -qO- --header="X-Auth-Token: $GITHUB_TOKEN" https://api.github.com/repos/TheGoddessInari/Hamsket/releases|grep browser_download_url|grep amd64.deb|head -n1|cut -d '"' -f4)"
-dpkg-deb -x Hamsket*.deb hamsket
-dpkg-deb -e Hamsket*.deb hamsket/DEBIAN
-rm -rf Hamsket*.deb
+dpkg-deb -x hamsket*.deb hamsket
+dpkg-deb -e hamsket*.deb hamsket/DEBIAN
+rm -rf hamsket*.deb
 npx asar e hamsket/opt/Hamsket/resources/app.asar app-hamsket
 cat <<EOF |tee -a  app-hamsket/resources/Hamsket-all.css
 @media (prefers-color-scheme:dark) {
