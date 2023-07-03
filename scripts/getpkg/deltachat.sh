b926c7e4328f86058cee56d951f7fb2fdee6c9e5
Raul Dipeas <rdipeas+github@proton.me>
2023-07-03 14:05:18 -0300

    ^scripts

--- scripts/getpkg/deltachat.sh
+++ scripts/getpkg/deltachat.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 # Delta Chat
-wget -q --show-progress "$(wget -qO- https://delta.chat/en/download|grep deb|cut -d '"' -f 2)"
+wget -q --show-progress "$(wget -qO- https://delta.chat/en/download|grep deb|cut -d '"' -f 2|head -n1)"
