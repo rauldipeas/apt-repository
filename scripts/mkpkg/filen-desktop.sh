5db15c86a83664f665e5db423163725c71b033f2
Raul Dipeas <rdipeas+github@proton.me>
2023-07-19 22:13:42 -0300

    ^scripts

--- scripts/mkpkg/filen-desktop.sh
+++ scripts/mkpkg/filen-desktop.sh
@@ -8,7 +8,7 @@ mkdir -p filen-desktop/{DEBIAN,opt,usr/share/applications}
 mv squashfs-root/filen-desktop.desktop filen-desktop/usr/share/applications/
 sed -i 's@AppRun --no-sandbox@/opt/filen-desktop/filen-desktop --no-sandbox@g' filen-desktop/usr/share/applications/filen-desktop.desktop
 mv squashfs-root/usr/share/icons filen-desktop/usr/share/
-rm -r squashfs-root/{AppRun,usr}
+rm -r squashfs-root/{.DirIcon,AppRun,usr}
 mv squashfs-root filen-desktop/opt/filen-desktop
 cp assets/filen-desktop/*.png filen-desktop/opt/filen-desktop/resources/app/src/assets/icons/tray/
 FILEN_VERSION="$(cat filen-desktop/opt/filen-desktop/resources/app/package.json|grep version|head -n1|cut -d '"' -f4)"
@@ -18,7 +18,6 @@ Version: $FILEN_VERSION
 Depends: libayatana-appindicator3-1
 Architecture: amd64
 Maintainer:  Filen Cloud Dienste  <FilenCloudDienste@github.com>
-Section: kernel
 Priority: optional
 Description:  Desktop client for Filen.io
 EOF
