f76d838d96937bbc7310389e62c89b1d8f90b7a2
Raul Dipeas <rdipeas+github@proton.me>
2023-08-26 12:53:35 -0300

    ^stremio

--- scripts/getpkg/stremio.sh
+++ scripts/getpkg/stremio.sh
@@ -3,4 +3,5 @@ set -e
 # Stremio
 wget -q --show-progress	http://ftp.debian.org/debian/pool/non-free/f/fdk-aac/"$(wget -qO- http://ftp.debian.org/debian/pool/non-free/f/fdk-aac/|grep libfdk-aac1|grep amd64.deb|head -n2|tail -n1|cut -d '"' -f8)"
 wget -q --show-progress http://security.ubuntu.com/ubuntu/pool/main/o/openssl/"$(wget -qO- http://security.ubuntu.com/ubuntu/pool/main/o/openssl/|grep libssl1.1_|grep amd64.deb|head -n1|cut -d '"' -f8)"
-wget -q --show-progress "$(wget -qO- https://www.stremio.com/downloads|grep amd64.deb|cut -d '"' -f248)"
+wget -q --show-pwget -q --show-progress "$(wget -qO- https://www.stremio.com/downloads|grep amd64.deb|cut -d '"' -f248)"
+rogress "$(wget -qO- https://www.stremio.com/downloads|grep amd64.deb|cut -d '"' -f286)"
