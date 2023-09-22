93cf5e693981537612fd8a8f954fb631a0592896
Raul Dipeas <rdipeas+github@proton.me>
2023-09-22 00:18:41 -0300

    ^anytype

--- scripts/getpkg/anytype.sh
+++ scripts/getpkg/anytype.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
 # Anytype
-wget -q --show-progress --content-disposition https://download.anytype.io/"$(wget -qO- https://download.anytype.io/|grep Debian|cut -d '"' -f2)"
+#wget -q --show-progress --content-disposition https://download.anytype.io/"$(wget -qO- https://download.anytype.io/|grep Debian|cut -d '"' -f2)"
+wget -q --show-progress "$(wget -qO- --header="X-Auth-Token: $GITHUB_TOKEN" https://api.github.com/repos/anyproto/anytype-ts/releases|grep browser_download_url|grep .deb|head -n1|cut -d '"' -f4)"
