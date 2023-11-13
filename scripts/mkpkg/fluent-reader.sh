1a8af8bf2f6f1a153e927161655ddfbb970eba55
Raul Dipeas <rdipeas+github@proton.me>
2023-11-13 01:41:19 +0000

    ^fluent-reader

--- scripts/mkpkg/fluent-reader.sh
+++ scripts/mkpkg/fluent-reader.sh
@@ -5,7 +5,7 @@ git clone -q https://aur.archlinux.org/fluent-reader-bin
 cd fluent-reader-bin
 sed -i 's/depends/#depends/g' PKGBUILD
 sed -i 's/-bin//g' PKGBUILD
-makedeb
+makedeb -s
 cd ..
 mv fluent-reader-bin/fluent-reader*.deb .
 rm -rf fluent-reader-bin
