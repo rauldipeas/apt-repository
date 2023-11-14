a4c2741e04248ac8dee8f5229f1a863961b1e917
Raul Dipeas <rdipeas+github@proton.me>
2023-11-14 14:01:39 +0000

    ^env

--- scripts/env.sh
+++ scripts/env.sh
@@ -18,7 +18,8 @@ sudo rm -r\
 sudo rm -r /etc/apt/sources.list.d/*.list
 #sudo add-apt-repository -y ppa:kisak/kisak-mesa
 echo 'deb http://ppa.launchpad.net/kisak/kisak-mesa/ubuntu jammy main'|sudo tee /etc/apt/sources.list.d/kisak-mesa.list
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 90935439
+#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 90935439
+cat assets/kisak-mesa/key.gpg|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/kisak-mesa.gpg
 echo 'deb [arch=all] https://proget.hunterwittenborn.com makedeb main'|sudo tee /etc/apt/sources.list.d/makedeb.list>/dev/null
 wget -qO- 'https://proget.hunterwittenborn.com/debian-feeds/makedeb.pub'|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/makedeb-archive-keyring.gpg>/dev/null
 sudo dpkg --add-architecture i386
