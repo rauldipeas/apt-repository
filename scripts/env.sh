92cfd09abf5af7bd55fa2fd6c27ff21b38334f76
Raul Dipeas <rdipeas+github@proton.me>
2023-12-02 19:25:36 +0000

    -obs-multi-rtmp ^env

--- scripts/env.sh
+++ scripts/env.sh
@@ -19,7 +19,6 @@ sudo rm -r /etc/apt/sources.list.d/*.list
 #sudo add-apt-repository -y ppa:kisak/kisak-mesa
 echo 'deb http://ppa.launchpad.net/kisak/kisak-mesa/ubuntu jammy main'|sudo tee /etc/apt/sources.list.d/kisak-mesa.list>/dev/null
 #sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 90935439
-#echo "$KISAK_MESA_GPG_KEY"|sudo tee /etc/apt/trusted.gpg.d/kisa-mesa.gpg>/dev/null
 echo "$KISAK_MESA_GPG_KEY"|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/kisak-mesa.gpg>/dev/null
 echo 'deb [arch=all] https://proget.hunterwittenborn.com makedeb main'|sudo tee /etc/apt/sources.list.d/makedeb.list>/dev/null
 #wget -qO- 'https://proget.hunterwittenborn.com/debian-feeds/makedeb.pub'|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/makedeb-archive-keyring.gpg>/dev/null
