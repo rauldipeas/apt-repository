57a5c3377c1150789707d2a9c35a8d2c82093663
Raul Dipeas <rauldipeas@disroot.org>
2024-03-26 09:21:09 -0300

    ^authy

--- scripts/mkpkg/authy.sh
+++ scripts/mkpkg/authy.sh
@@ -4,8 +4,8 @@ set -e
 sudo apt install -y squashfs-tools
 mkdir -p authy
 cd authy
-wget -qO- 'https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=authy-electron'|grep -v ^depends|tee PKGBUILD>/dev/null
-wget -qO- 'https://aur.archlinux.org/cgit/aur.git/plain/no-sandbox.sh?h=authy-electron'|tee no-sandbox.sh>/dev/null
+wget -qO- 'https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=authy'|grep -v ^depends|tee PKGBUILD>/dev/null
+wget -qO- 'https://aur.archlinux.org/cgit/aur.git/plain/no-sandbox.sh?h=authy'|tee no-sandbox.sh>/dev/null
 makedeb -H 'Section: security'
 cd ..
 mv authy/authy*.deb .
