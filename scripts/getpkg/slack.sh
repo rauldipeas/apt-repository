96fbf1ad2e5c7b48e14ed96dd9459247667b5dd8
Raul Dipeas <rauldipeas@disroot.org>
2024-05-07 23:21:11 -0300

    -slack

--- scripts/getpkg/slack.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-# Slack
-SLACK_VER="$(wget -qO- https://slack.com/intl/pt-br/downloads/linux|grep -o "Versão *.*"|cut -d '<' -f1|sed 's/Versão //g')"
-wget -q --show-progress https://downloads.slack-edge.com/releases/linux/$SLACK_VER/prod/x64/slack-desktop-$SLACK_VER-amd64.deb
