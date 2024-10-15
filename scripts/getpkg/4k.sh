ecf0de086e7fbbd15c137c7bd8395384338dafc0
Raul Dipeas <rauldipeas@disroot.org>
2024-10-15 09:37:28 -0300

    -4k -cpupower-gui

--- scripts/getpkg/4k.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-# 4K
-wget -qO- https://www.4kdownload.com/pt-br/downloads|grep amd64.deb|cut -d '"' -f4|cut -d '?' -f1|xargs wget -q --show-progress
--- scripts/getpkg/cpupower-gui.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-# cpupower-gui
-wget -q --show-progress "$(wget -qO- https://api.github.com/repos/vagnum08/cpupower-gui/releases|grep browser_download_url|grep all.deb|head -n1|cut -d '"' -f4)"
