bae6b5084f79bdcbf21996ebfe1647c415821868
Raul Dipeas <rauldipeas@disroot.org>
2024-10-16 15:41:44 -0300

    ^build

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,6 +18,7 @@ done
 #fury packages|sed 's/deb   /@/g'|sed 's/public//g'|sed 's/ //g'|sed 's/\*\*\*GEMFURYPACKAGES\*\*\*//g'|sed 's/namekindversionprivacy//g'|tee fury-packages>/dev/null;xargs fury yank --force <fury-packages
 
 # Packagecloud
+set
 for PACKAGE in *.deb
 do
     package_cloud push rauldipeas/deb/debian/bookworm "$PACKAGE"
