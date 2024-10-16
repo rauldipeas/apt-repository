0f9a0383e0a21309b08cda014e025f24f12a01a3
Raul Dipeas <rauldipeas@disroot.org>
2024-10-16 15:08:06 -0300

    ^build ^env

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,5 +15,10 @@ for PACKAGE in *.deb
 do
     curl -sF package=@"$PACKAGE" https://"$GEMFURY_PUSH_TOKEN"@push.fury.io/rauldipeas/
 done
-
 #fury packages|sed 's/deb   /@/g'|sed 's/public//g'|sed 's/ //g'|sed 's/\*\*\*GEMFURYPACKAGES\*\*\*//g'|sed 's/namekindversionprivacy//g'|tee fury-packages>/dev/null;xargs fury yank --force <fury-packages
+
+# Packagecloud
+for PACKAGE in *.deb
+do
+    package_cloud push rauldipeas/deb "$PACKAGE"
+done
