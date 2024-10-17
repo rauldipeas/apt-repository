ac150a19511001b60cf2d54e4b1062c3187574f8
Raul Dipeas <rauldipeas@disroot.org>
2024-10-16 21:19:03 -0300

    ^apt-repository ^build

--- scripts/build.sh
+++ scripts/build.sh
@@ -22,3 +22,9 @@ for PACKAGE in *.deb
 do
     curl -sF "package[distro_version_id]=215" -F "package[package_file]=@$PACKAGE" https://"$PACKAGECLOUD_TOKEN":@packagecloud.io/api/v1/repos/rauldipeas/deb/packages.json
 done
+
+# BaltoRepo
+for PACKAGE in *.deb
+do
+    curl --verbose --header "Authorization: Bearer $BALTOREPO_TOKEN" --form "package=@$PACKAGE" --form "distribution=all" https://rauldipeas.baltorepo.com/debian-rdx/debian-rdx/upload/
+done
