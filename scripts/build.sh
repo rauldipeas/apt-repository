7617fbf474ba1b8f93fccf963519c4f360f46807
Raul Dipeas <github.rauldipeas@lock.email>
2024-11-26 07:38:59 -0300

    ^build

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,13 +18,13 @@ done
 #fury packages|sed 's/deb   /@/g'|sed 's/public//g'|sed 's/ //g'|sed 's/\*\*\*GEMFURYPACKAGES\*\*\*//g'|sed 's/namekindversionprivacy//g'|tee fury-packages>/dev/null;xargs fury yank --force <fury-packages
 
 # Packagecloud
-for PACKAGE in *.deb
-do
-    curl -sF "package[distro_version_id]=215" -F "package[package_file]=@$PACKAGE" https://"$PACKAGECLOUD_TOKEN":@packagecloud.io/api/v1/repos/rauldipeas/deb/packages.json
-done
+#for PACKAGE in *.deb
+#do
+#    curl -sF "package[distro_version_id]=215" -F "package[package_file]=@$PACKAGE" https://"$PACKAGECLOUD_TOKEN":@packagecloud.io/api/v1/repos/rauldipeas/deb/packages.json
+#done
 
 # BaltoRepo
-for PACKAGE in *.deb
-do
-    curl -s --header "Authorization: Bearer $BALTOREPO_TOKEN" --form "package=@$PACKAGE" --form "distribution=all" https://rauldipeas.baltorepo.com/debian-rdx/debian-rdx/upload/
-done
+#for PACKAGE in *.deb
+#do
+#    curl -s --header "Authorization: Bearer $BALTOREPO_TOKEN" --form "package=@$PACKAGE" --form "distribution=all" https://rauldipeas.baltorepo.com/debian-rdx/debian-rdx/upload/
+#done
