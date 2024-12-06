f425a6d53372afe4091ab7eea3bbeffd3816a7e6
Raul Dipeas <github.rauldipeas@lock.email>
2024-12-06 18:40:25 -0300

    ^apt-repository

--- apt-repository.sh
+++ apt-repository.sh
@@ -5,7 +5,7 @@ cat <<EOF |sudo tee /etc/apt/sources.list.d/rauldipeas.sources>/dev/null
 X-Repolib-Name: Raul Dipeas
 Enabled: yes
 Types: deb
-URIs: https://rauldipeas.fury.site/apt
+URIs: https://apt.rauldipeas.com.br
 Suites: *
 Components: *
 Trusted: yes
