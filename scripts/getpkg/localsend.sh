e3d32ee68e047b34fddc46fb7db24eb5216f8305
Raul Dipeas <github.rauldipeas@lock.email>
2025-01-10 13:56:55 -0300

    +localsend

--- scripts/getpkg/localsend.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-# LocalSend
-wget -q --show-progress "$(wget -qO- https://api.github.com/repos/localsend/localsend/releases|grep browser_download_url|grep x86-64.deb|head -n1|cut -d '"' -f4)"
