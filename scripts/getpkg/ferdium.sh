de70175cfda63883da47a86f9b0213bb67f33d4c
Raul Dipeas <github.rauldipeas@lock.email>
2025-02-27 23:23:49 -0300

    -ferdium -tonelib -waveterm

--- scripts/getpkg/ferdium.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-# Ferdium
-wget -q --show-progress "$(wget -qO- https://api.github.com/repos/ferdium/ferdium-app/releases|grep browser_download_url|grep .deb|grep -v nightly|head -n1|cut -d '"' -f4)"
--- scripts/getpkg/waveterm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-set -e
-wget -q --show-progress "$(wget -qO- https://api.github.com/repos/wavetermdev/waveterm/releases|grep browser_download_url|grep amd64|grep deb|head -n1|cut -d '"' -f4)"
